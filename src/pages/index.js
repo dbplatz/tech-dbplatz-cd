60dfd78c81f7e4a69a66c8c5a04293da161fa5ad
unknown <support@dbplatz.com>
2020-07-08 15:04:01 +0100

    Maintenance Website 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "DBplatz"
+    const siteTitle = "Blog @ DBplatz"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -22,7 +22,7 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Our website is currently undergoing scheduled maintenance.</p>
+        <p>Our blog is currently undergoing scheduled maintenance.</p>
         {/* <p>Welcome to your new Tech Gatsby website. You are on your home page.</p> */}
         {/* <p>
           This starter comes out of the box with styled components and Gatsby's
